a2dca6749fa0cbabceb68d36f777daf605e7cff4
Dragos Sabau <dragos.sabau@sincaibm.ro>
2021-12-19 10:11:47 +0200

    andromeda: Corrected Maintainer

--- cherish_andromeda.mk
+++ cherish_andromeda.mk
@@ -33,5 +33,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cherish.maintainer=Dragos
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=Copperhead
+	
