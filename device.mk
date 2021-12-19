4911bb0c8a2ad0a1d77ed1ee5adc77f46a7cfc3f
Dragos Sabau <dragos.sabau@sincaibm.ro>
2021-12-19 10:09:55 +0200

    andromeda: Added XiaomiParts again

--- device.mk
+++ device.mk
@@ -135,7 +135,7 @@ PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.0-service
 
 # Device-specific settings
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     XiaomiParts
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/parts/snd,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
